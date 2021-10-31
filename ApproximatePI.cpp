81d6ac3e44cae93d631fc4b04f3b5472972554ad
Hesston Robertson <hestonlolzerton@gmail.com>
2021-10-31 03:42:18 -0500

    Initial Commit
    Skeleton Code

---
+++ ApproximatePI.cpp
@@ -0,0 +1,12 @@
+﻿// ApproximatePI.cpp : Defines the entry point for the application.
+//
+
+#include "ApproximatePI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
